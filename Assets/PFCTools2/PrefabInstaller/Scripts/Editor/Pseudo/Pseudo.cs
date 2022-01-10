7484d96dd505a01fa1a2ba407467283746f3816d
PF_Cactus <brandonvdongen@hotmail.com>
2022-01-10 15:37:24 +0100

    Expression Parameter auto insert/removal.

--- Assets/PFCTools2/PrefabInstaller/Scripts/Editor/Pseudo/Pseudo.cs
+++ Assets/PFCTools2/PrefabInstaller/Scripts/Editor/Pseudo/Pseudo.cs
@@ -7,9 +7,8 @@ using UnityEditor.Animations;
 using System;
 using PFCTools2.Utils;
 
-#if VRC_SDK_VRCSDK3
 using VRC.SDK3.Avatars.Components;
-#endif
+using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace PFCTools2.Installer.PseudoParser {
 
@@ -23,32 +22,25 @@ namespace PFCTools2.Installer.PseudoParser {
 
 
         public static List<PseudoAction> EnabledActions = new List<PseudoAction> { new PseudoLayer(), new PseudoEntry(), new PseudoTransition(), new PseudoState(), new PseudoAny(), new PseudoEntry(), new PseudoExit(), new PseudoParameter() };
-#if VRC_SDK_VRCSDK3
-        public static List<PseudoAction> VRCActions = new List<PseudoAction> { new PseudoVRCParameterDriver() };
-#endif
+        public static List<PseudoAction> EnabledVRCActions = new List<PseudoAction> { new PseudoVRCParameterDriver() };
         public static List<PseudoAction> getEnabledActions() {
             List<PseudoAction> availableActions = new List<PseudoAction>();
             availableActions.AddRange(EnabledActions);
-#if VRC_SDK_VRCSDK3
-            availableActions.AddRange(VRCActions);
-#endif
+            availableActions.AddRange(EnabledVRCActions);
             return availableActions;
         }
-        public static void Parse(TextAsset asset, AnimatorController controller = null) {
+        public static void Parse(TextAsset asset, AvatarDefinition currentAvatar, AnimatorController controller = null) {
             string path = AssetDatabase.GetAssetPath(asset);
-            Parse(Lexxer(asset), path, controller);
+            Parse(Lexxer(asset), path, currentAvatar, controller);
         }
-        public static void Parse(List<Token> tokenList, string path, AnimatorController controller = null) {
-#if VRC_SDK_VRCSDK3
-            EnabledActions.AddRange(VRCActions);
-#endif
+        public static void Parse(List<Token> tokenList, string path, AvatarDefinition currentAvatar, AnimatorController controller = null) {
             TokenStream Tokens = new TokenStream(tokenList);
-            ControllerContext context;
+            ControllerContext controllerContext;
             if (controller) {
-                context = new ControllerContext(controller);
+                controllerContext = new ControllerContext(controller);
             }
             else {
-                context = new ControllerContext(path);
+                controllerContext = new ControllerContext(path);
             }
             while (!Tokens.EOF()) {
                 Token token = Tokens.Next();
@@ -59,7 +51,7 @@ namespace PFCTools2.Installer.PseudoParser {
                 bool foundAction = false;
                 foreach (PseudoAction action in getEnabledActions()) {
                     if (token.value == action.ActionKey) {
-                        context = action.Process(context, Tokens);
+                        controllerContext = action.Process(controllerContext, Tokens, currentAvatar);
                         foundAction = true;
                         break;
                     }
@@ -70,6 +62,94 @@ namespace PFCTools2.Installer.PseudoParser {
 
         }
 
+        public static void Remove(TextAsset asset, AvatarDefinition currentAvatar, AnimatorController controller) {
+            string path = AssetDatabase.GetAssetPath(asset);
+            Remove(Lexxer(asset), path, currentAvatar, controller);
+        }
+        public static void Remove(List<Token> tokenList, string path, AvatarDefinition currentAvatar, AnimatorController controller) {
+            TokenStream Tokens = new TokenStream(tokenList);
+            ControllerContext controllerContext;
+            if (controller == null) {
+                throw new Exception("No controller was given, aborting.");
+            }
+            else {
+                controllerContext = new ControllerContext(controller);
+            }
+            while (!Tokens.EOF()) {
+                Token token = Tokens.Next();
+                if ((token.type & TokenType.Action) == TokenType.Mismatch) {
+                    Tokens.Exception();
+                }
+
+                bool foundAction = false;
+                foreach (PseudoAction action in getEnabledActions()) {
+                    if (token.value == action.ActionKey) {
+                        controllerContext = action.Remove(controllerContext, Tokens, currentAvatar);
+                        foundAction = true;
+                        break;
+                    }
+                }
+                if (!foundAction) Tokens.Exception();
+                if (controller) EditorUtility.SetDirty(controller);
+            }
+
+        }
+
+        /*
+        public static void RemoveLayers(TextAsset asset, AvatarDefinition currentAvatar, AnimatorController controller) {
+            string path = AssetDatabase.GetAssetPath(asset);
+            RemoveLayers(Lexxer(asset), path, currentAvatar, controller);
+        }
+        public static void RemoveLayers(List<Token> tokenList, string path, AvatarDefinition currentAvatar, AnimatorController controller) {
+            string layerKey = new PseudoLayer().ActionKey;
+            string parameterKey = new PseudoParameter().ActionKey;
+
+            TokenStream Tokens = new TokenStream(tokenList);
+            while (!Tokens.EOF()) {
+                Token token = Tokens.Next();
+                if (token.value == layerKey) {
+                    string layerName = Tokens.Next().value;
+                    Debug.Log("Found layer: "+ layerName);
+
+                    for (var i = 0; i < controller.layers.Length; i++) {
+                        if(controller.layers[i].name == layerName) {
+                            controller.RemoveLayer(i);
+                            EditorUtility.SetDirty(controller);
+                            break;
+                        }
+                    }
+
+                }
+                else if(token.value == parameterKey) {
+                    string parameterName = Tokens.Next().value;
+                    Debug.Log("Found parameter: " + parameterName + " in animator, removing...");
+
+                    for(var i = 0; i < controller.parameters.Length; i++) {
+                        if(controller.parameters[i].name == parameterName) {
+                            controller.RemoveParameter(i);
+                            EditorUtility.SetDirty(controller);
+
+                            List<VRCExpressionParameters.Parameter> parameters = new List<VRCExpressionParameters.Parameter>(currentAvatar.ExpressionParameters.parameters);
+                            VRCExpressionParameters.Parameter param = currentAvatar.ExpressionParameters.FindParameter(parameterName);
+                            if (param != null) {
+                                Debug.Log("Found Parameter: " + parameterName + " in expression parameters, removing...");
+                                parameters.Remove(param);
+                                currentAvatar.ExpressionParameters.parameters = parameters.ToArray();
+                                EditorUtility.SetDirty(currentAvatar.ExpressionParameters);
+                            }
+                            break;
+                        }
+                        
+                    }
+                }
+                
+                
+                
+
+            }
+
+        }
+        */
         internal static void Export(AnimatorController animatorController) {
             string export = "";
             ControllerContext Context = new ControllerContext(animatorController);
--- Assets/PFCTools2/PrefabInstaller/Scripts/Editor/Pseudo/actions/PseudoParameter.cs
+++ Assets/PFCTools2/PrefabInstaller/Scripts/Editor/Pseudo/actions/PseudoParameter.cs
@@ -1,27 +1,82 @@
-﻿using UnityEditor.Animations;
+﻿using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEditor;
+using PFCTools2.Utils;
 #if VRC_SDK_VRCSDK3
 using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Avatars.ScriptableObjects;
 #endif
 
 namespace PFCTools2.Installer.PseudoParser {
     public class PseudoParameter : PseudoAction {
         public override string ActionKey => "parameter";
-        public override ControllerContext Process(ControllerContext Context, TokenStream Tokens) {
-            Token nametoken = Tokens.Next(TokenType.String);
-            Token valueToken = Tokens.Next(TokenType.String);
+        public override ControllerContext Process(ControllerContext context, TokenStream tokenStream, AvatarDefinition currentAvatar) {
+            Token nametoken = tokenStream.Next(TokenType.String);
+            Token valueToken = tokenStream.Next(TokenType.String);
+            Token syncToken = tokenStream.Peek();
+
+            Debug.Log(currentAvatar.ExpressionParameters.name);
+
+            List<VRCExpressionParameters.Parameter> parameters = new List<VRCExpressionParameters.Parameter>();
+            if (currentAvatar != null) parameters = new List<VRCExpressionParameters.Parameter>(currentAvatar.ExpressionParameters.parameters);
+            VRCExpressionParameters.Parameter newParam = new VRCExpressionParameters.Parameter();
+
             if (valueToken.value == "float") {
-                Context.GetParameter(nametoken.value, AnimatorControllerParameterType.Float);
+                context.GetParameter(nametoken.value, AnimatorControllerParameterType.Float);
+                newParam.valueType = VRCExpressionParameters.ValueType.Float;
             }
             else if (valueToken.value == "int") {
-                Context.GetParameter(nametoken.value, AnimatorControllerParameterType.Int);
+                context.GetParameter(nametoken.value, AnimatorControllerParameterType.Int);
+                newParam.valueType = VRCExpressionParameters.ValueType.Int;
             }
             else if (valueToken.value == "bool") {
-                Context.GetParameter(nametoken.value, AnimatorControllerParameterType.Bool);
+                context.GetParameter(nametoken.value, AnimatorControllerParameterType.Bool);
+                newParam.valueType = VRCExpressionParameters.ValueType.Bool;
             }
+            if (syncToken.value == "sync" && currentAvatar != null) {
+                tokenStream.Next();
+                if (currentAvatar.ExpressionParameters.FindParameter(nametoken.value) == null) {
+                    newParam.name = nametoken.value;
+                    parameters.Add(newParam);
+                    currentAvatar.ExpressionParameters.parameters = parameters.ToArray();
+                    EditorUtility.SetDirty(currentAvatar.ExpressionParameters);
+                }
+            }
+            return context;
+        }
 
-                return Context;
+        public override ControllerContext Remove(ControllerContext context, TokenStream tokenStream, AvatarDefinition currentAvatar) {
+            Token nameToken = tokenStream.Next(TokenType.String);//nameToken
+            Token valueToken = tokenStream.Next(TokenType.String);//ValueToken
+            Token syncToken = tokenStream.Peek();
+
+            AnimatorControllerParameter foundParameter = null;
+            foreach (AnimatorControllerParameter parameter in context.Controller.parameters) {
+                if(parameter.name == nameToken.value) {
+                    foundParameter = parameter;
+                    break;
+                }
+            }
+            if(foundParameter != null) {
+                context.Controller.RemoveParameter(foundParameter);
+            }
+            
+
+            if (syncToken.value == "sync") {
+                tokenStream.Next();
+
+                VRCExpressionParameters.Parameter parameter = currentAvatar.ExpressionParameters.FindParameter(nameToken.value);
+                if(parameter != null) { 
+
+                    List<VRCExpressionParameters.Parameter> parameters = new List<VRCExpressionParameters.Parameter>(currentAvatar.ExpressionParameters.parameters);
+                    parameters.Remove(parameter);
+                    currentAvatar.ExpressionParameters.parameters = parameters.ToArray();
+                    EditorUtility.SetDirty(currentAvatar.ExpressionParameters);
+                }
+
+            }
+            return context;
         }
     }
 }
