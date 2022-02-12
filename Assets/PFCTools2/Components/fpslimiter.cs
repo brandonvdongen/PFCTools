def6d29799f7fe8bc37bfb60f8d6cf2efd25c5a3
PF_Cactus <brandonvdongen@hotmail.com>
2022-02-12 08:40:29 +0100

    Added FPS Limiter Component

---
+++ Assets/PFCTools2/Components/fpslimiter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpslimiter : MonoBehaviour
+{
+    [Range(30,90)]
+    public int target = 30;
+
+    void Awake() {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    void Update() {
+        if (Application.targetFrameRate != target)
+            Application.targetFrameRate = target;
+    }
+}
