9fe9c209d314045025eb6806b7d1cc734b539bb5
PF_Cactus <brandonvdongen@hotmail.com>
2022-02-14 06:58:11 +0100

    Update AvatarAssetCollector.cs

--- Assets/PFCTools2/AvatarTools/Editor/AvatarAssetCollector.cs
+++ Assets/PFCTools2/AvatarTools/Editor/AvatarAssetCollector.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEditor.Animations;
+using UnityEngine.Animations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using VRC.SDK3.Avatars.Components;
 using VRC.SDK3.Avatars.ScriptableObjects;
 using PFCTools2.Utils;
 
+
 namespace PFCTools2.AvatarTools {
     public class AvatarAssetCollector {
-        [MenuItem("GameObject/PFCTools/Sort Avatar Files", false, 0)]
-        static void HierarchyExport() {
+        [MenuItem("GameObject/PFCTools/Folderize Asset Dependencies", false, 0)]
+        static void HierarchyExport() { 
             ExportAvatar();
         }
-        [MenuItem("GameObject/PFCTools/Sort Avatar Files", true, 0)]
+        [MenuItem("GameObject/PFCTools/Folderize Asset Dependencies", true, 0)]
         static bool HierarchyValidation() {
             return ValidateIfAvatar();
         }
 
-        [MenuItem("Assets/PFCTools/Sort Avatar Files", false, 303)]
+        [MenuItem("Assets/PFCTools/Folderize Asset Dependencies", false, 303)]
         static void ProjectExport() {
             ExportAvatar();
         }
 
-        [MenuItem("Assets/PFCTools/Sort Avatar Files", true, 303)]
+        [MenuItem("Assets/PFCTools/Folderize Asset Dependencies", true, 303)]
         static bool ProjectValidate() {
             return ValidateIfAvatar();
         }
@@ -38,12 +40,13 @@ namespace PFCTools2.AvatarTools {
                 VRCAvatarDescriptor descriptor = go.GetComponent<VRCAvatarDescriptor>();
                 isValid = (descriptor != null);
             }
-            return isValid;
+            //return isValid;
+            return true;
         }
 
         private class DirectoryTable {
 
-            public string rootPath;
+            public string rootPath = "";
             private bool _allowVRCSDKFiles = false;
             private bool _VRCSDKChecked = false;
 
@@ -54,6 +57,8 @@ namespace PFCTools2.AvatarTools {
             private Dictionary<string, string> _paths = new Dictionary<string, string>();
             public Dictionary<string, string> Paths { get => _paths; }
             public void Add(string orgPath, string destPath = "") {
+                Debug.Log($"{orgPath} > {destPath}");
+                if (Path.GetDirectoryName(orgPath) == $"Assets/{rootPath}/{destPath}") return;
                 if (orgPath.Contains("VRCSDK")) {
                     if (orgPath.Contains("proxy_")) return;
                     if (!_VRCSDKChecked) {
@@ -63,14 +68,21 @@ namespace PFCTools2.AvatarTools {
                     if (!_allowVRCSDKFiles) return;
 
                 }
+                
                 if (_paths.ContainsKey(orgPath)) {
                     if (_paths[orgPath] == destPath) {
                         string newPath = Path.GetDirectoryName(destPath);
-                        _paths[orgPath] = $"Assets/{rootPath}/{newPath}/Shared";
+                        if(rootPath != "")_paths[orgPath] = $"Assets/{rootPath}/{newPath}/Shared";
+                        else _paths[orgPath] = $"Assets/{rootPath}/{newPath}/Shared";
+                        
                     }
                 }
                 else {
-                    _paths.Add(orgPath, $"Assets/{rootPath}/{destPath}");
+                    Debug.Log($"wtf: {orgPath} | Assets/{rootPath}/{destPath}");
+                    string path = "Assets";
+                    if (rootPath.Length > 0) path += "/" + rootPath;
+                    if (destPath.Length > 0) path += "/" + destPath;
+                    _paths.Add(orgPath, path);
                 }
             }
             public void Add(Object _object, string destPath = "") {
@@ -95,15 +107,15 @@ namespace PFCTools2.AvatarTools {
 
             GameObject prefab = Selection.activeGameObject as GameObject;
             DirectoryTable directories = new DirectoryTable(prefab.name);
-
-            EditorUtility.DisplayProgressBar("Fetching Avatar Files", "Processing Avatar.", 0);
+            bool cancel;
+            cancel = EditorUtility.DisplayCancelableProgressBar("Fetching Avatar Files", "Processing Avatar.", 0);
 
 
             //See if the Selected object is part of a scene asset, if so make it the root.
             Object root = prefab;
             if (prefab.scene != null) {
                 if (prefab.scene.path != "" && prefab.scene.path != null) {
-                    directories.Add(prefab.scene.path);
+                    directories.Add(prefab.scene.path, "");
                     root = AssetDatabase.LoadAssetAtPath<Object>(prefab.scene.path);
                 }
                 else {
@@ -125,7 +137,7 @@ namespace PFCTools2.AvatarTools {
                 else if(_renderer is MeshRenderer) {
                     MeshFilter filter = _renderer.gameObject.GetComponent<MeshFilter>();
                     if(filter != null) {
-                        directories.Add(filter.sharedMesh);
+                        directories.Add(filter.sharedMesh,"Models");
                         
                     }
                 }
@@ -166,9 +178,25 @@ namespace PFCTools2.AvatarTools {
                 maxDepth--;
             }
 
-            AnimatorController controller = prefab.GetComponent<Animator>().runtimeAnimatorController as AnimatorController;
-            if (controller) {
-                ProcessAnimatorController(directories, controller);
+            //Hanlde Animator Controller
+            Animator animator = prefab.GetComponent<Animator>();
+            if (animator) {
+                AnimatorController controller = animator.runtimeAnimatorController as AnimatorController;
+                if (controller) {
+                    ProcessAnimatorController(directories, controller);
+                }
+            }
+            //Find potential world constraints
+            
+            foreach(IConstraint constraint in prefab.GetComponentsInChildren<IConstraint>()) {
+                List<ConstraintSource> sources = new List<ConstraintSource>();
+                constraint.GetSources(sources);
+
+                foreach(var source in sources) {
+                    if(source.sourceTransform.gameObject.scene.path == null) {
+                        directories.Add(source.sourceTransform.gameObject, "Extras");
+                    }
+                }
             }
 
             //VRCDescriptor Files
@@ -195,7 +223,7 @@ namespace PFCTools2.AvatarTools {
             float i = 1;
             List<string> _directoryList = new List<string>();
             foreach (string value in directories.Paths.Values) {
-                EditorUtility.DisplayProgressBar("Compiling folder structure", value, i / directories.Paths.Count);
+                cancel = EditorUtility.DisplayCancelableProgressBar("Compiling folder structure", value, i / directories.Paths.Count);
 
                 if (!_directoryList.Contains(value)) {
                     _directoryList.Add(value);
@@ -205,30 +233,36 @@ namespace PFCTools2.AvatarTools {
 
             i = 1;
             foreach (string dir in _directoryList) {
-                EditorUtility.DisplayProgressBar("Creating Directories", dir, i / _directoryList.Count);
+                cancel = EditorUtility.DisplayCancelableProgressBar("Creating Directories", dir, i / _directoryList.Count);
                 Directory.CreateDirectory($"{dir}");
                 i++;
             }
             AssetDatabase.Refresh();
 
+            List<string> lines = new List<string>();
+
             i = 1;
             foreach (var path in directories.Paths.Keys) {
-                EditorUtility.DisplayProgressBar("Moving Avatar Files", path, i / directories.Paths.Keys.Count);
+                EditorUtility.DisplayCancelableProgressBar("Moving Avatar Files", path, i / directories.Paths.Keys.Count);
                 string fileName = Path.GetFileName(path);
-                AssetDatabase.MoveAsset(path, $"{directories.Paths[path]}/{fileName}");
+                string destPath = $"{directories.Paths[path]}/{fileName}";
+                AssetDatabase.MoveAsset(path, destPath);
+                if(path != destPath)lines.Add($"{lines.Count}: {path} >>> {destPath}");
                 i++;
             }
+            if(lines.Count > 0)File.WriteAllLines(Path.GetDirectoryName(AssetDatabase.GetAssetPath(root)) + "/MoveManifest.txt",lines);
             Selection.activeObject = root;
             EditorUtility.ClearProgressBar();
             System.TimeSpan Time = System.DateTime.Now.Subtract(startTime);
             Debug.Log($"Processed avatar: {prefab.name} Took {Time.TotalMinutes:0}:{Time.Seconds:00}Minutes");
+            AssetDatabase.Refresh();
         }
 
 
         //////////////////// METHODS //////////////////////////
 
         private static void ProcessExpressionsMenu(DirectoryTable directories, VRCExpressionsMenu menu) {
-            directories.Add(menu, "VRC/Menus/");
+            directories.Add(menu, "VRC/Menus");
             if (menu != null) {
                 foreach (VRCExpressionsMenu.Control control in menu.controls) {
                     if(control.icon != null) {
@@ -236,7 +270,7 @@ namespace PFCTools2.AvatarTools {
                     }
                     if(control.subMenu != null) {
                         //Debug.Log(path);
-                        directories.Add(control.subMenu, "VRC/Menus/");
+                        directories.Add(control.subMenu, "VRC/Menus");
                         //if (path != "") path = path + "/" + control.subMenu.name;
                         //else path = control.subMenu.name;
                         ProcessExpressionsMenu(directories, control.subMenu);
@@ -247,6 +281,8 @@ namespace PFCTools2.AvatarTools {
         }
 
         private static void getTexturesFromMaterial(DirectoryTable directories, Material _material) {
+            //TODO MOVE THIS
+            directories.Add(_material.shader, "Shaders");
             foreach (string name in _material.GetTexturePropertyNames()) {
                 Texture _texture = _material.GetTexture(name);
                 if (_texture != null) {
@@ -260,34 +296,45 @@ namespace PFCTools2.AvatarTools {
             directories.Add(controller, "Controllers");
             if (controller.animationClips.Length > 0) {
                 foreach (var layer in controller.layers) {
-                    foreach (var childState in layer.stateMachine.states) {
-                        Motion anim = childState.state.motion;
-                        if (anim == null) continue;
-                        processMotion(directories, layer.name, anim);
-                    }
+                    if(layer.stateMachine.name == "")layer.stateMachine.name = layer.name;
+                    ProcessStateMachine(directories, layer.stateMachine);
                 }
             }
         }
 
-        private static void processMotion(DirectoryTable directories, string path, Motion motion) {
-            if (motion is AnimationClip) {
-                processAnimationClip(directories, path, motion as AnimationClip);
+        private static void ProcessStateMachine(DirectoryTable directories, AnimatorStateMachine stateMachine, string path = "") {
+            
+            path = path == "" ? stateMachine.name : $"{path}/{stateMachine.name}";
+            foreach (var childState in stateMachine.states) {
+                Motion anim = childState.state.motion;
+                if (anim == null) continue;
+                processMotion(directories, anim, path);
             }
-            else if (motion is BlendTree) {
-                processBlendTree(directories, path, motion as BlendTree);
+
+            foreach (var childStateMachine in stateMachine.stateMachines) {
+                ProcessStateMachine(directories, childStateMachine.stateMachine, path);
             }
         }
 
-        private static void processBlendTree(DirectoryTable directories, string path, BlendTree blendTree) {
+        private static void processMotion(DirectoryTable directories, Motion motion, string path = "") {
+            if (motion is AnimationClip) {
+                processAnimationClip(directories, motion as AnimationClip, path);
+            }
+            else if (motion is BlendTree) {
+                processBlendTree(directories, motion as BlendTree, path);
+            }
+        }
+
+        private static void processBlendTree(DirectoryTable directories, BlendTree blendTree, string path = "") {
             if (AssetDatabase.IsMainAsset(blendTree)) {
                 directories.Add(blendTree, $"Animations/{path}/{blendTree.name}");
             }
             foreach (var childMotion in blendTree.children) {
-                processMotion(directories, path+"/"+blendTree.name, childMotion.motion);
+                processMotion(directories, childMotion.motion, path + "/" + blendTree.name);
             }
         }
 
-        private static void processAnimationClip(DirectoryTable directories, string path, AnimationClip anim) {
+        private static void processAnimationClip(DirectoryTable directories, AnimationClip anim, string path = "") {
             if (path != "" && path != null) directories.Add(anim, $"Animations/{path}");
             else directories.Add(anim, "Animations");
             EditorCurveBinding[] bindings = AnimationUtility.GetObjectReferenceCurveBindings(anim);
@@ -300,25 +347,6 @@ namespace PFCTools2.AvatarTools {
                 }
 
             }
-        }
-
-        public static void moveFiles(List<Object> files, string assetPath) {
-            foreach (Object file in files) {
-                string path = AssetDatabase.GetAssetPath(file);
-                string fileName = Path.GetFileName(path);
-                if (assetPath == "") {
-                    AssetDatabase.MoveAsset(path, $"Assets/{fileName}");
-                }
-                else {
-                    AssetDatabase.MoveAsset(path, $"Assets/{assetPath}/{fileName}");
-                }
-
-            }
-        }
-        public static void moveFiles(Object file, string assetPath) {
-            List<Object> files = new List<Object>();
-            files.Add(file);
-            moveFiles(files, assetPath);
         }
     }
 }
