8882b2c7b48f8ba6691040dade92f99ff749063a
PF_Cactus <brandonvdongen@hotmail.com>
2022-05-09 03:05:49 +0200

    Added Physbone Utils
    Added: Shared Root PhysBone Splitter.
    Added: Avatar Physbone Collector.

---
+++ Assets/PFCTools2/CleanupTools/Editor/PhysBoneUtils.cs
@@ -0,0 +1,109 @@
+﻿using PFCTools2.Utils;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+
+namespace PFCTools2.PhysboneUtils
+{
+    public static class PhysBoneUtils
+    {
+        #region Separate Physbones To Children
+        [MenuItem("PFCTools2/PhysBone Utilities/Separate Bones Into Children")]
+        public static void SeparatePhysonesToChildren()
+        {
+            VRCPhysBone[] bones = Selection.activeGameObject.GetComponentsInChildren<VRCPhysBone>();
+            foreach (VRCPhysBone bone in bones)
+            {
+                GameObject ga = new GameObject(bone.GetRootTransform().name);
+                VRCPhysBone newBone = ga.AddComponent<VRCPhysBone>();
+                ga.transform.SetParent(bone.transform, false);
+
+                ClassCopier.Copy<VRCPhysBone>(bone, newBone);
+                Object.DestroyImmediate(bone);
+            }
+        }
+
+        [MenuItem("PFCTools2/PhysBone Utilities/Separate Bones Into Children", true)]
+        public static bool Validate()
+        {
+            if (Selection.activeGameObject.GetComponentInChildren<VRCPhysBone>() != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Collect All Physbones to Selected Parent
+        [MenuItem("PFCTools2/PhysBone Utilities/Collect All Physbones")]
+        public static void CollectPhysBones()
+        {
+            GameObject container = new GameObject("Container");
+            VRCAvatarDescriptor descriptor = Selection.activeGameObject.GetComponentInParent<VRCAvatarDescriptor>();
+            if (descriptor == null) { throw new System.Exception("No Avatar Root Found."); }
+
+            VRCPhysBone[] bones = descriptor.gameObject.GetComponentsInChildren<VRCPhysBone>();
+            Dictionary<Transform, Transform> gameObjects = new Dictionary<Transform, Transform>();
+            foreach (VRCPhysBone bone in bones)
+            {
+                Transform root = bone.GetRootTransform();
+                HashSet<Transform> path = new HashSet<Transform>();
+
+                Transform current = root;
+                while (current != null && current != descriptor.transform)
+                {
+
+                    if (!gameObjects.ContainsKey(current))
+                    {
+                        GameObject go = new GameObject(current.name);
+                        go.transform.parent = container.transform;
+                        gameObjects.Add(current, go.transform);
+                    }
+                    current = current.parent;
+                }
+                if (gameObjects.ContainsKey(root))
+                {
+                    VRCPhysBone newBone = gameObjects[root].gameObject.AddComponent<VRCPhysBone>();
+                    ClassCopier.Copy(bone, newBone);
+                    newBone.rootTransform = root;
+                    GameObject.DestroyImmediate(bone);
+                }
+            }
+            foreach (Transform key in gameObjects.Keys)
+            {
+                if (gameObjects.ContainsKey(key.parent))
+                {
+                    gameObjects[key].parent = gameObjects[key.parent];
+                }
+                else
+                {
+                    gameObjects[key].parent = Selection.activeTransform;
+                }
+            }
+            GameObject.DestroyImmediate(container.gameObject);
+
+        }
+
+        [MenuItem("PFCTools2/PhysBone Utilities/Collect All Physbones", true)]
+        public static bool CollectPhysBonesValidate()
+        {
+            VRCAvatarDescriptor desc = Selection.activeGameObject.GetComponentInParent<VRCAvatarDescriptor>();
+            if (desc == null)
+            {
+                return false;
+            }
+
+            if (desc.gameObject.GetComponentInChildren<VRCPhysBone>() != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ Assets/PFCTools2/Lib/Editor/ClassCopier.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace PFCTools2.Utils
+{
+    public class ClassCopier
+    {
+        public static void Copy<T>(T sourceComp, T targetComp)
+        {
+            FieldInfo[] sourceFields = sourceComp.GetType().GetFields(BindingFlags.Public |
+                                                             BindingFlags.NonPublic |
+                                                             BindingFlags.Instance);
+            int i = 0;
+            for (i = 0; i < sourceFields.Length; i++)
+            {
+                object value = sourceFields[i].GetValue(sourceComp);
+                sourceFields[i].SetValue(targetComp, value);
+            }
+        }
+    }
+}
